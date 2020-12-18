cc8d449e3f9914ad44d5555709e3bd8f4bc01d90
Christopher Lamm <chris@theantichris.com>
2020-12-18 15:30:47 -0500

    wip: added pseudo code

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+type task struct{}
 
+type epic struct{}
+
+func main() {
+	// Setup webserver
+
+	// Create handler for taskStatusUpdated https://clickup20.docs.apiary.io/#reference/0/webhooks/create-webhook
+	// Get Task information
+	// Create Clubhouse Epic
+	// Send Epic to Clubhouse https://clubhouse.io/api/rest/v3/#Create-Epic
 }
