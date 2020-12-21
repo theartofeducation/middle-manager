870f1fb42171743173230a2cd5ee717d685fea63
Christopher Lamm <chris@theantichris.com>
2020-12-21 09:38:40 -0500

    test: test for rootHandler

--- handlers.go
+++ handlers.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+const rootResponse = `{"message": "Locke, I told you I need those TPS reports done by noon today."}`
+
 func rootHandler() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		writer.WriteHeader(http.StatusOK)
 		writer.Header().Set("Content-Type", "application/json")
 
-		io.WriteString(writer, `{"message": "Locke, I told you I need those TPS reports done by noon today."}`)
+		io.WriteString(writer, rootResponse)
 	}
 }
