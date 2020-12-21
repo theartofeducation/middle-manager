b39e4f99e29a97a5ace6ed02d82bff31a1005987
Christopher Lamm <chris@theantichris.com>
2020-12-21 12:54:41 -0500

    feature: parsing webhook body

--- handlers.go
+++ handlers.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
+
+	"github.com/pkg/errors"
 )
 
 const rootResponse = `{"message": "Locke, I told you I need those TPS reports done by noon today."}`
@@ -16,14 +19,30 @@ func rootHandler() http.HandlerFunc {
 }
 
 func taskStatusUpdatedHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, r *http.Request) {
-		// Verify signature
-		writer.WriteHeader(http.StatusOK)
+	return func(writer http.ResponseWriter, request *http.Request) {
+		writer.WriteHeader(http.StatusNoContent)
+
+		// TODO: Verify signature
+		signature := request.Header.Get("X-Signature")
+		log.Infoln("Signature: ", signature)
+
+		// key := os.Getenv("TASK_STATUS_UPDATED_SECRET")
+
+		var webhook Webhook
+		if err := json.NewDecoder(request.Body).Decode(&webhook); err != nil {
+			log.Errorln(errors.Wrap(err, "taskStatusUpdatedHandler"))
+		}
 
 		// TODO: Get Task information
 		// TODO: Create Clubhouse Epic
 		// TODO: Send Epic to Clubhouse https://clubhouse.io/api/rest/v3/#Create-Epic
-
-		io.WriteString(writer, `{"message": "It works!"}`)
 	}
+}
+
+// Webhook holds the information for a webhook from ClickUp.
+type Webhook struct {
+	ID     string `json:"webhook_id"`
+	Event  string
+	TaskID string `json:"task_id"`
+	// history_items
 }
