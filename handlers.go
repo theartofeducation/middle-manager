d7402babe15c16ab79a842d67c986143f71b316a
Christopher Lamm <chris@theantichris.com>
2020-12-22 09:53:01 -0500

    feature: verifying signature

--- handlers.go
+++ handlers.go
@@ -1,14 +1,24 @@
 package main
 
 import (
-	"encoding/json"
+	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"io"
+	"io/ioutil"
 	"net/http"
+	"os"
 
 	"github.com/pkg/errors"
 )
 
 const rootResponse = `{"message": "Locke, I told you I need those TPS reports done by noon today."}`
+
+// Custom errors.
+var (
+	ErrSignatureMismatch = errors.New("Signature mismatch")
+)
 
 func rootHandler() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
@@ -20,19 +30,28 @@ func rootHandler() http.HandlerFunc {
 
 func taskStatusUpdatedHandler() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
-		// TODO: filter to list https://app.clickup.com/2324163/v/li/52581966
 		writer.WriteHeader(http.StatusNoContent)
 
-		// TODO: Verify signature
-		signature := request.Header.Get("X-Signature")
-		log.Infoln("Signature: ", signature)
+		clickUpSignature := request.Header.Get("X-Signature")
+		secret := []byte(os.Getenv("TASK_STATUS_UPDATED_SECRET"))
+		body := getBody(request)
 
-		// key := os.Getenv("TASK_STATUS_UPDATED_SECRET")
+		hash := hmac.New(sha256.New, secret)
+		hash.Write(body)
+		generatedSignature := hex.EncodeToString(hash.Sum(nil))
 
-		var webhook Webhook
-		if err := json.NewDecoder(request.Body).Decode(&webhook); err != nil {
-			log.Errorln(errors.Wrap(err, "taskStatusUpdatedHandler"))
+		if clickUpSignature != generatedSignature {
+			log.Errorln(errors.Wrap(ErrSignatureMismatch, "taskStatusUpdatedHandler"))
+
+			writer.WriteHeader(http.StatusUnauthorized)
+
+			return
 		}
+
+		// var webhook Webhook
+		// if err := json.NewDecoder(request.Body).Decode(&webhook); err != nil {
+		// 	log.Errorln(errors.Wrap(err, "taskStatusUpdatedHandler"))
+		// }
 
 		// TODO: Get Task information
 		// TODO: Create Clubhouse Epic
@@ -45,5 +64,13 @@ type Webhook struct {
 	ID     string `json:"webhook_id"`
 	Event  string
 	TaskID string `json:"task_id"`
-	// history_items
+}
+
+func getBody(request *http.Request) (bodyBytes []byte) {
+	if request.Body != nil {
+		bodyBytes, _ = ioutil.ReadAll(request.Body)
+		request.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
+	}
+
+	return bodyBytes
 }
