c530ffa87a4a613793598b206d7aaad2d03112e7
Christopher Lamm <chris@theantichris.com>
2020-12-29 13:42:48 -0500

    tests: initial happy path test

--- handlers_test.go
+++ handlers_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+
+	"github.com/sirupsen/logrus/hooks/test"
+	"github.com/theartofeducation/middle-manager/clickup"
+	"github.com/theartofeducation/middle-manager/clubhouse"
 )
 
 func Test_rootHandler(t *testing.T) {
@@ -19,4 +24,28 @@ func Test_rootHandler(t *testing.T) {
 	if response.Body.String() != rootResponse {
 		t.Errorf("response returned wrong body: got %q want %q", response.Body.String(), rootResponse)
 	}
+}
+
+func Test_taskStatusUpdatedHandler(t *testing.T) {
+	t.Run("it creates an Epic when Task is updated to Ready For Development", func(t *testing.T) {
+		logger, _ := test.NewNullLogger()
+		app = App{
+			log:       logger,
+			clickup:   clickup.MockClient{},
+			clubhouse: clubhouse.MockClient{},
+		}
+
+		body := []byte(`{}`)
+		request, _ := http.NewRequest(http.MethodPost, "/task-status-updated", bytes.NewBuffer(body))
+		request.Header.Add("X-Signature", "abc123")
+
+		response := httptest.NewRecorder()
+
+		handler := http.HandlerFunc(taskStatusUpdatedHandler())
+		handler.ServeHTTP(response, request)
+
+		if response.Code != http.StatusNoContent {
+			t.Errorf("respones return wrong status: got %d want %d", response.Code, http.StatusNoContent)
+		}
+	})
 }
